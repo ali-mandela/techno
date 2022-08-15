94a635f730cb23e5d341e97235e3bc24fdb90150
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-08-15 23:52:39 +0600

    header sized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
       <Header></Header>
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+import logo from '../assets/logo-1.png'
 
 const Header = () => {
     return (
-        <div>
-            <div class="navbar bg-base-100">
+        <div className='mx-20'>
+            <div class="navbar sticky bg-base-100">
                 <div class="flex-1">
-                    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a class="btn btn-ghost normal-case text-xl ">
+                        <img src={logo} alt="logo" />
+                    </a>
                 </div>
                 <div class="flex-none">
                     <ul class="menu menu-horizontal p-0">
-                        <li><a>Item 1</a></li>
+                        <li><Link to='/' className='  text-xl'>Home</Link></li>
+                        <li><Link to='/about' className='  text-xl'>About Us</Link></li>
                         <li tabindex="0">
-                            <a>
-                                Parent
+                            <a className='  text-xl'>
+                                Services
                                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                             </a>
                             <ul class="p-2 bg-base-100">
@@ -20,7 +26,16 @@ const Header = () => {
                                 <li><a>Submenu 2</a></li>
                             </ul>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li><Link to='/contact' className='  text-xl'>Contact Us</Link></li>
+                        {/* <ul class=" menu-horizontal">
+                            <li><NavLink to='/' style={({ isActive }) =>
+                                (isActive ? { color: '#D7F561' } : { color: 'White' })} className='text-white font-bold px-8'>Home</NavLink></li>
+                            <li><NavLink to='/service' style={({ isActive }) =>
+                                (isActive ? { color: '#D7F561' } : { color: 'White' })} className='text-white font-bold  px-8'>Service</NavLink></li>
+                            <li><NavLink to='/login' style={({ isActive }) =>
+                                (isActive ? { color: '#D7F561' } : { color: 'White' })} className='text-white font-bold  px-8'>Login</NavLink></li>
+
+                        </ul> */}
                     </ul>
                 </div>
             </div>
