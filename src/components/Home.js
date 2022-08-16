afb596958a97fff4e3d1d07870024e4c1ee5f9bb
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-08-17 00:41:23 +0600

    offer part done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Header from './Header';
+import Offer from './Offer';
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Banner />
+            <Offer />
         </div>
     );
 };
