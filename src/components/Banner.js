43068bf9b963210c5fb9026fa475951b8a286e8b
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-08-20 23:41:41 +0600

    add all flipping card

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Banner.css'
 import sap from '../assets/SAP-1-min.png'
+import micro from '../assets/microsoft.png'
+import sf from '../assets/salesforce.png'
+import oracle from '../assets/oracle.png'
 import homeBanner from "../assets/Hom-banner-Image-01-min-scaled.jpg"
 
 const Banner = () => {
@@ -17,24 +20,42 @@ const Banner = () => {
                         know more
                     </button>
                 </div>
-                <div>
+                <div className='grid grid-cols-2 '>
 
                     <div className='card'>
                         <div className='face front'>
                             <img className='img' src={sap} alt="" />
                         </div>
                         <div className='face back'>
-                            <h1>SAP</h1>
+                            <h1 className='font-semibold font-xl'>SAP</h1>
                             <p>SAP HANA | SAP FIORI | SAP ERP</p>
                         </div>
                     </div>
                     <div className='card'>
                         <div className='face front'>
-                            <img className='img' src={sap} alt="" />
+                            <img className='img' src={micro} alt="" />
                         </div>
                         <div className='face back'>
-                            <h1>SAP</h1>
-                            <p>SAP HANA | SAP FIORI | SAP ERP</p>
+                            <h1 className='font-semibold font-xl'>MICROSOFT</h1>
+                            <p>Manage your customer database, lead Management, customers engagement and resolve customer service issues.</p>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='face front'>
+                            <img className='img' src={sf} alt="" />
+                        </div>
+                        <div className='face back'>
+                            <h1 className='font-semibold font-xl'>Salesforce</h1>
+                            <p>Advance CRM to MAnage customer database, lead Management service issue. Sales Module, Marketing Modules.</p>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='face front'>
+                            <img className='img' src={oracle} alt="" />
+                        </div>
+                        <div className='face back'>
+                            <h1 className='font-semibold font-xl'>ORACLE</h1>
+                            <p>Manage databases, implement and customize Oracle ERP products.</p>
                         </div>
                     </div>
                 </div>
